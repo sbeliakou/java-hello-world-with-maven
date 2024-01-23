b1fa14abe4be9258b73876f6e0907604524b4917
Siarhei Beliakou <sbeliakou@gmail.com>
2024-01-23 16:46:07 +0200

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+        return "Sample Java Project for Playpit Labs";
     }
 }
